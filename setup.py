8b65be3a562cbaa849d2dafd02da8a8e0f98993b
phephik <josef.novotny96@seznam.cz>
2017-09-22 18:04:12 +0200

    Update setup.py
    Version 0.2.0

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='ws2812_server',
-    version='0.1.0',
+    version='0.2.0',
     description="Python server providing API remote access to control WS2812.",
     long_description=readme + '\n\n' + history,
     author="Josef Novotny",
