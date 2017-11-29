889bb9f494a9928af0ae09a16465e8fd89fa30a7
phephik <josef.novotny96@seznam.cz>
2017-11-29 13:02:26 +0100

    Update cli.py

--- ws2812_server/cli.py
+++ ws2812_server/cli.py
@@ -13,7 +13,7 @@ def ws2812server(config_file):
     """Console script for ws2812_server."""
     set = yaml.load(config_file)
     srv = Ws2812ApiServer(settings=set)
-    srv.run(host='0.0.0.0', port=8080)
+    srv.run(host='0.0.0.0', port=80)
 
 if __name__ == "__main__":
     ws2812server()
